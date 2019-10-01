f1a5767c85dc7d5478f3349f74f19a8f90ea9255
defunctl <justin.frydman@gmail.com>
2019-10-01 10:12:13 -0600

    detect touch screens before initializing slim

--- register.js
+++ register.js
@@ -25,7 +25,27 @@ $(function () {
         return element.offsetWidth > 0 && element.offsetHeight > 0;
     }
 
-    if (!/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent)) {
+    function detectTouchscreen() {
+        var result = false;
+        if (window.PointerEvent && ('maxTouchPoints' in navigator)) {
+            // if Pointer Events are supported, just check maxTouchPoints
+            if (navigator.maxTouchPoints > 0) {
+                result = true;
+            }
+        } else {
+            // no Pointer Events...
+            if (window.matchMedia && window.matchMedia("(any-pointer:coarse)").matches) {
+                // check for any-pointer:coarse which mostly means touchscreen
+                result = true;
+            } else if (window.TouchEvent || ('ontouchstart' in window)) {
+                // last resort - check for exposed touch events API / event handler
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    if (!detectTouchscreen()) {
         var slim = [];
 
         $('.select select').each(function() {
