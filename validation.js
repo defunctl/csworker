bf411f6c264af684c906b38d94bed26299d23055
defunctl <justin.frydman@gmail.com>
2019-12-08 13:06:25 -0700

    Add custom HTML5 validation

---
+++ validation.js
@@ -0,0 +1,16 @@
+$(function () {
+    var $registerInputs = $('#register input[type=text]');
+
+    $registerInputs.on('invalid', function () {
+        console.log($(this));
+        if ($(this).get(0).validity.valueMissing) {
+            $(this).get(0).setCustomValidity('Please fill out this field');
+        } else {
+            $(this).get(0).setCustomValidity('Please enter no more than 128 characters');
+        }
+    });
+
+    $registerInputs.on('input, change', function () {
+        $(this).get(0).setCustomValidity('');
+    });
+});
